443f8137f3c6bf685142c8fefbd8ea208aaf44f3
Carolina Alves <carol.lima.alves2005@gmail.com>
2024-04-20 21:38:07 -0300

    galeria de imagens página consoles

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,43 @@
 // Ativar marcador da página atual
-const links = document.querySelectorAll('.header-menu a')
+const links = document.querySelectorAll(".header-menu a");
 
-function ativarLink(link){
-  const url = location.href
-  const href = link.href
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
 
-  if (url.includes(href)){
-    link.classList.add('ativo')
+  if (url.includes(href)) {
+    link.classList.add("ativo");
   }
 }
 
-links.forEach(ativarLink)
+links.forEach(ativarLink);
 
 // Ativar items selecionados no orçamento
-const parametros = new URLSearchParams(location.search)
+const parametros = new URLSearchParams(location.search);
 
-function selecionarParametros(parametro){
-  const elemento = document.getElementById(parametro)
-  if (elemento){
-    elemento.checked = true
+function selecionarParametros(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
   }
 }
 
-parametros.forEach(selecionarParametros)
+parametros.forEach(selecionarParametros);
+
+// Galeria de imagens
+const imagens = document.querySelectorAll(".console-imagens img");
+const galeria = document.querySelector(".console-imagens");
+
+function trocarImagem(event) {
+  const atual = event.currentTarget;
+  const media = matchMedia("(min-width:1000px)").matches;
+  if (media) {
+    galeria.prepend(atual);
+  }
+}
+
+function eventosImagems(imagem) {
+  imagem.addEventListener("click", trocarImagem);
+}
+
+imagens.forEach(eventosImagems);
